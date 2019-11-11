6e5712c9d8e48111cce0560818bc928cf34a2cdd
ZelenuX <44063918+ZelenuX@users.noreply.github.com>
2019-11-11 10:15:18 +0700

    Add files via upload

--- lab1/DNA.h
+++ lab1/DNA.h
@@ -1,18 +1,18 @@
 #pragma once
+#include <unordered_map>
 #include <iostream>
-using namespace std;
 
 enum Nucleotide { A, G, C, T };
 
 class RNA {
 	char* nucs = nullptr;
-	unsigned int length = 0;
-	unsigned int allocatedLength = 0;
+	size_t length = 0;
+	size_t allocatedLength = 0;
 	class NucleotideReference {
 		RNA* rna;
-		unsigned int idx;
+		size_t idx;
 	public:
-		NucleotideReference(RNA* rna, unsigned int idx) {
+		NucleotideReference(RNA* rna, size_t idx) {
 			this->rna = rna;
 			this->idx = idx;
 		}
@@ -27,8 +27,7 @@ class RNA {
 			case 3:
 				return T;
 			default:
-				cout << "Error in Nucleotide() in nucleotideReference.\n";	//todo change
-				return G;
+				return A;
 			}
 		}
 		Nucleotide operator=(Nucleotide nucleotide) {
@@ -54,10 +53,14 @@ class RNA {
 			return nuc;
 		}
 	};
-	unsigned int newBytesAmount(unsigned int newLength) {
-		unsigned int resBytesAmount;
-		unsigned int curBytesAmount = allocatedLength / 4 + (allocatedLength % 4 ? 1 : 0);
-		unsigned int newBytesAmount = newLength / 4 + (newLength % 4 ? 1 : 0);
+private:
+	inline size_t filledBytes() {
+		return (length / 4 + (bool)(length % 4));
+	}
+	size_t newBytesAmount(size_t newLength) {
+		size_t resBytesAmount;
+		size_t curBytesAmount = allocatedLength / 4 + (bool)(allocatedLength % 4);
+		size_t newBytesAmount = newLength / 4 + (bool)(newLength % 4);
 		if (curBytesAmount >= newBytesAmount) {
 			if (newBytesAmount * 4 > curBytesAmount) {
 				resBytesAmount = curBytesAmount;
@@ -67,75 +70,112 @@ class RNA {
 			}
 		}
 		else {
-			unsigned int shift = 0;
+			size_t shift = 0;
 			--newBytesAmount;
 			while (newBytesAmount > 0) {
 				newBytesAmount >>= 1;
 				++shift;
 			}
-			resBytesAmount = (unsigned int)1 << shift;
+			resBytesAmount = (size_t)1 << shift;
 		}
 		return (resBytesAmount < 4 ? 4 : resBytesAmount);
 	}
-	void allocMem(unsigned int rnaLength) {
+	void allocMem(size_t rnaLength) {
 		allocatedLength = newBytesAmount(rnaLength);
 		nucs = new char[allocatedLength];
 		allocatedLength *= 4;
 		length = rnaLength;
 	}
-	void resize(unsigned int newLength) {
-		if (allocatedLength == newBytesAmount(newLength))
+	void resize(size_t newLength, bool copy = true) {
+		if (allocatedLength == newBytesAmount(newLength) * 4) {
+			length = newLength;
 			return;
+		}
 		char* oldNucs = nucs;
-		unsigned int copyLength = newLength < length ? newLength : length;
+		size_t copyLength = newLength / 4 + (bool)(newLength % 4);
+		if (filledBytes() < copyLength)
+			copyLength = filledBytes();
 		allocMem(newLength);
-		for (unsigned int i = 0; i != copyLength / 4; ++i)
-			nucs[i] = oldNucs[i];
-		if (copyLength % 4)
-			nucs[copyLength / 4] = oldNucs[copyLength / 4];
+		if (copy && copyLength > 0) {
+			for (size_t i = 0; i != copyLength; ++i)
+				nucs[i] = oldNucs[i];
+		}
 		delete[] oldNucs;
 	}
 public:
-	RNA() {}
-	explicit RNA(unsigned int rnaLength) {
+	RNA() = default;
+	explicit RNA(size_t rnaLength) {
 		allocMem(rnaLength);
 	}
-	RNA(Nucleotide nucleotide, unsigned int rnaLength) : RNA(rnaLength) {
-		for (unsigned int i = 0; i != rnaLength; ++i)
-			(*this)[i] = nucleotide;
+	RNA(Nucleotide nucleotide, size_t rnaLength) : RNA(rnaLength) {
+		size_t byteAmount = filledBytes();
+		RNA tmpRna(4);
+		tmpRna[0] = tmpRna[1] = tmpRna[2] = tmpRna[3] = nucleotide;
+		char fillingByte = tmpRna.nucs[0];
+		for (size_t i = 0; i != byteAmount; ++i)
+			this->nucs[i] = fillingByte;
 	}
-	RNA(RNA& rna) : RNA(rna.length) {
-		for (unsigned int i = 0; i != length; ++i)
-			(*this)[i] = rna[i];
+	RNA(const RNA& rna) : RNA(rna.length) {
+		if (length == 0)
+			return;
+		for (size_t i = filledBytes() - 1; i > 0; --i)
+			nucs[i] = rna.nucs[i];
+		nucs[0] = rna.nucs[0];
+	}
+	RNA(RNA&& rna) {
+		this->allocatedLength = rna.allocatedLength;
+		this->length = rna.length;
+		this->nucs = rna.nucs;
+		rna.nucs = nullptr;
+		rna.allocatedLength = rna.length = 0;
 	}
 	~RNA() {
 		delete[] nucs;
+		nucs = nullptr;
+		length = allocatedLength = 0;
 	}
-	unsigned int size() {
+	size_t size() {
 		return length;
 	}
-	unsigned int capacity() {
+	size_t capacity() {
 		return allocatedLength;
 	}
 	RNA operator+(RNA& rna2) {
 		RNA rna(length + rna2.length);
-		for (unsigned int i = 0; i != length; ++i)
-			rna[i] = (*this)[i];
-		for (unsigned int i = 0; i != rna2.length; ++i)
-			rna[length + i] = rna2[i];
+		if (length > 0) {
+			for (size_t i = filledBytes() - 1; i > 0; --i)
+				rna.nucs[i] = this->nucs[i];
+			rna.nucs[0] = this->nucs[0];
+		}
+		if (rna2.length > 0) {
+			for (size_t i = 0; i != rna2.length; ++i)
+				rna[length + i] = rna2[i];
+		}
 		return rna;
 	}
 	RNA operator+(Nucleotide nucleotide) {
 		RNA rna(length + 1);
-		for (unsigned int i = 0; i != length; ++i)
-			rna[i] = (*this)[i];
+		if (length != 0) {
+			for (size_t i = filledBytes() - 1; i > 0; --i)
+				rna.nucs[i] = this->nucs[i];
+			rna.nucs[0] = this->nucs[0];
+		}
 		rna[length] = nucleotide;
 		return rna;
+	}
+	RNA& operator+=(Nucleotide nucleotide) {
+		this->resize(this->length + 1);
+		(*this)[this->length - 1] = nucleotide;
+		return *this;
+	}
+	RNA& operator+=(RNA& rna) {
+		*this = *this + rna;
+		return *this;
 	}
 	bool operator!=(RNA& rna2) {
 		if (length != rna2.length)
 			return true;
-		for (unsigned int i = 0; i != length / 4; ++i)
+		for (size_t i = 0; i != length / 4; ++i)
 			if (nucs[i] != rna2.nucs[i])
 				return true;
 		for (char i = 0; i != length % 4; ++i)
@@ -149,16 +189,27 @@ public:
 	RNA& operator=(RNA& rna2) {
 		if (&rna2 == this)
 			return rna2;
-		resize(rna2.length);
+		resize(rna2.length, false);
 		for (int i = 0; i != length / 4; ++i)
 			nucs[i] = rna2.nucs[i];
 		if (length % 4)
 			nucs[length / 4] = rna2.nucs[length / 4];
 		return rna2;
 	}
+	RNA& operator=(RNA&& rna2) {
+		if (this == &rna2)
+			return *this;
+		delete[] this->nucs;
+		this->allocatedLength = rna2.allocatedLength;
+		this->length = rna2.length;
+		this->nucs = rna2.nucs;
+		rna2.nucs = nullptr;
+		rna2.allocatedLength = rna2.length = 0;
+		return *this;
+	}
 	RNA operator!() {
-		RNA rna(*this);
-		for (unsigned int i = 0; i != size(); ++i)
+		RNA rna(this->size());
+		for (size_t i = 0; i != size(); ++i)
 			switch ((*this)[i]) {
 			case A:
 				rna[i] = T;
@@ -175,13 +226,16 @@ public:
 			}
 		return rna;
 	}
-	NucleotideReference operator[](unsigned int idx) {
+	NucleotideReference operator[](size_t idx) {
 		return NucleotideReference(this, idx);
+	}
+	Nucleotide operator[](size_t idx) const {
+		return (*const_cast<RNA*>(this))[idx];
 	}
 	bool isComplementary(RNA& rna) {
 		if (rna.length != length)
 			return false;
-		for (unsigned int i = 0; i != length; ++i)
+		for (size_t i = 0; i != length; ++i)
 			switch ((*this)[i]) {
 			case A:
 				if (rna[i] != T)
@@ -202,63 +256,41 @@ public:
 			}
 		return true;
 	}
-	RNA split(unsigned int tailStartIdx) {
+	RNA split(size_t tailStartIdx) {
+		if (tailStartIdx >= length)
+			return RNA();
 		RNA tail(length - tailStartIdx);
-		for (unsigned int i = 0; i != length - tailStartIdx; ++i)
+		for (size_t i = 0; i != length - tailStartIdx; ++i)
 			tail[i] = (*this)[tailStartIdx + i];
 		resize(tailStartIdx);
 		return tail;
 	}
-	unsigned int cardinality(Nucleotide nucleotide) {
-		unsigned int res = 0;
+	size_t cardinality(Nucleotide nucleotide) {
+		size_t res = 0;
 		for (int i = 0; i != length; ++i)
 			res += ((*this)[i] == nucleotide);
 		return res;
 	}
-	void print() {
-		for (unsigned int i = 0; i != this->length; ++i)
-			switch ((*this)[i]) {
-			case A:
-				cout << 'A';
-				break;
-			case G:
-				cout << 'G';
-				break;
-			case C:
-				cout << 'C';
-				break;
-			case T:
-				cout << 'T';
-				break;
-			}
-	}
-	void println() {
-		print();
-		cout << endl;
+	std::unordered_map<Nucleotide, size_t, std::hash<int>> cardinality() {
+		std::unordered_map<Nucleotide, size_t, std::hash<int>> res;
+		res[A] = cardinality(A);
+		res[C] = cardinality(C);
+		res[G] = cardinality(G);
+		res[T] = cardinality(T);
+		return res;
 	}
 };
 
 class DNA {
 private:
 	RNA rna0, rna1;
+	friend std::ostream& operator<<(std::ostream& stream, DNA& dna);
 public:
 	DNA(RNA& rna0, RNA& rna1) {
 		if (rna0.isComplementary(rna1)) {
 			this->rna0 = rna0;
 			this->rna1 = rna1;
 		}
-	}
-	void print() {
-		cout << '/';
-		rna0.print();
-		cout << '\\' << endl;
-		cout << '\\';
-		rna1.print();
-		cout << '/';
-	}
-	void println() {
-		print();
-		cout << endl;
 	}
 };
 
@@ -267,4 +299,26 @@ RNA operator+(Nucleotide nuc, RNA& rna) {
 }
 RNA operator+(Nucleotide nuc1, Nucleotide nuc2) {
 	return RNA(nuc1, 1) + nuc2;
+}
+std::ostream& operator<<(std::ostream& stream, RNA& rna) {
+	for (size_t i = 0; i != rna.size(); ++i)
+		switch (rna[i]) {
+		case A:
+			stream << 'A';
+			break;
+		case G:
+			stream << 'G';
+			break;
+		case C:
+			stream << 'C';
+			break;
+		case T:
+			stream << 'T';
+			break;
+		}
+	return stream;
+}
+std::ostream& operator<<(std::ostream& stream, DNA& dna) {
+	stream << '/' << dna.rna0 << "\\\n\\" << dna.rna1 << '/';
+	return stream;
 }
--- lab1/test.cpp
+++ lab1/test.cpp
@@ -1,5 +1,8 @@
 #pragma once
 #include "pch.h"
+#include <crtdbg.h>
+#define new new( _NORMAL_BLOCK, __FILE__, __LINE__)
+#define _CRTDBG_MAP_ALLOC
 #include "DNA.h"
 
 TEST(RNATest, constructors) {
@@ -11,7 +14,7 @@ TEST(RNATest, constructors) {
 	RNA rna1(size);
 	EXPECT_TRUE(rna1.capacity() >= size);
 	EXPECT_EQ(rna1.size(), size);
-
+	
 	size = 5;
 	Nucleotide nuc = C;
 	RNA rna2(nuc, size);
@@ -25,6 +28,23 @@ TEST(RNATest, constructors) {
 	EXPECT_EQ(rna3.size(), rna2.size());
 	for (unsigned int i = 0; i != rna2.size(); ++i)
 		EXPECT_TRUE(rna3[i] == rna2[i]);
+
+	RNA rna4(RNA(RNA(RNA(RNA(A, 7)))) + RNA(A, 8));
+	EXPECT_EQ(rna4.size(), 15);
+	EXPECT_TRUE(rna4 == RNA(A, 15));
+}
+TEST(RNATest, destructor) {
+	RNA rna(A, 9);
+	EXPECT_EQ(rna.size(), 9);
+	EXPECT_EQ(rna.capacity(), 16);
+	
+	rna.~RNA();
+	EXPECT_EQ(rna.size(), 0);
+	EXPECT_EQ(rna.capacity(), 0);
+
+	rna.~RNA();
+	EXPECT_EQ(rna.size(), 0);
+	EXPECT_EQ(rna.capacity(), 0);
 }
 TEST(RNATest, operatorSum) {
 	unsigned int size1 = 12;
@@ -32,13 +52,14 @@ TEST(RNATest, operatorSum) {
 	RNA rna1(T, size1);
 	RNA rna2(C, size2);
 
-	rna2 = rna2 + A + G;
-	EXPECT_TRUE(rna2.size() == size2 + 2);
+	rna2 = T + rna2 + (A + G);
+	EXPECT_EQ(rna2.size(), size2 + 3);
 	EXPECT_TRUE(rna2.capacity() >= rna2.size());
-	for (unsigned int i = 0; i != size2; ++i)
+	EXPECT_TRUE(rna2[0] == T);
+	for (unsigned int i = 1; i != size2; ++i)
 		EXPECT_TRUE(rna2[i] == C);
-	EXPECT_TRUE(rna2[size2] == A);
-	EXPECT_TRUE(rna2[size2 + 1] == G);
+	EXPECT_TRUE(rna2[size2 + 1] == A);
+	EXPECT_TRUE(rna2[size2 + 2] == G);
 
 	RNA rna3 = rna1 + rna2;
 	EXPECT_TRUE(rna3.size() == rna2.size() + rna1.size());
@@ -47,6 +68,14 @@ TEST(RNATest, operatorSum) {
 		EXPECT_TRUE(rna3[i] == rna1[i]);
 	for (unsigned int i = 0; i != rna2.size(); ++i)
 		EXPECT_TRUE(rna3[i + rna1.size()] == rna2[i]);
+}
+TEST(RNATest, operatorInc) {
+	RNA rna0(A, 10);
+	rna0 += G;
+	EXPECT_TRUE(rna0 == RNA(A, 10) + G);
+
+	rna0 += rna0;
+	EXPECT_TRUE(rna0 == RNA(A, 10) + G + RNA(A, 10) + G);
 }
 TEST(RNATest, operatorsEqNotEq) {
 	RNA rna1(A, 14);
@@ -64,7 +93,7 @@ TEST(RNATest, operatorAssignment) {
 	RNA rna0(C, 127);
 	RNA rna1(rna0);
 	RNA rna2(T, 257);
-	RNA rna3 = rna2 = rna1;
+	RNA rna3 = rna1 = rna2 = rna1;
 	EXPECT_TRUE(rna1 == rna0);
 	EXPECT_TRUE(rna2 == rna0);
 	EXPECT_TRUE(rna3 == rna0);
@@ -84,6 +113,8 @@ TEST(RNATest, operatorIdx) {
 	unsigned int size1 = 12;
 	unsigned int size2 = 10;
 	RNA rna0 = RNA(T, size1) + RNA(G, size2) + A;
+	const RNA rna2(A, 100);
+	Nucleotide n = rna2[0];
 	for (unsigned int i = 0; i != size1; ++i)
 		EXPECT_TRUE(rna0[i] == T);
 	for (unsigned int i = 0; i != size2; ++i)
@@ -101,6 +132,8 @@ TEST(RNATest, operatorIdx) {
 	EXPECT_TRUE(rna0[size1 + size2 - 2] == C);
 	EXPECT_TRUE(rna0[size1 + size2 - 1] == C);
 	EXPECT_TRUE(rna0[size1 + size2] == A);
+
+	EXPECT_TRUE(RNA(A, 1)[0] == RNA(A, 1)[0]);
 }
 TEST(RNATest, isComplementary) {
 	RNA rna0 = RNA(A, 4) + RNA(C, 3) + RNA(G, 2) + RNA(T, 1);
@@ -113,6 +146,23 @@ TEST(RNATest, split) {
 	RNA rna1 = rna0.split(6);
 	EXPECT_TRUE(rna0 == RNA(A, 4) + RNA(C, 2));
 	EXPECT_TRUE(rna1 == RNA(C, 1) + RNA(G, 2) + RNA(T, 1));
+	EXPECT_EQ(rna0.size(), 6);
+	EXPECT_EQ(rna1.size(), 4);
+
+	RNA rna2 = rna0;
+	rna0 = rna1.split(5);
+	EXPECT_EQ(rna0.size(), 0);
+	EXPECT_EQ(rna1.size(), 4);
+	rna2 = rna1.split(-3);
+	EXPECT_EQ(rna2.size(), 0);
+	EXPECT_EQ(rna1.size(), 4);
+	rna0 = rna1.split(0);
+	EXPECT_EQ(rna0.size(), 4);
+	EXPECT_EQ(rna1.size(), 0);
+
+	rna0 = rna1.split(0);
+	EXPECT_EQ(rna1.size(), 0);
+	EXPECT_EQ(rna0.size(), 0);
 }
 TEST(RNATest, cardinality) {
 	RNA rna0 = RNA(A, 4) + RNA(C, 3) + RNA(G, 2) + RNA(T, 1);
@@ -121,6 +171,11 @@ TEST(RNATest, cardinality) {
 	EXPECT_EQ(rna0.cardinality(C), 6);
 	EXPECT_EQ(rna0.cardinality(G), 9);
 	EXPECT_EQ(rna0.cardinality(T), 2);
+	std::unordered_map<Nucleotide, size_t, std::hash<int>> cards;
+	EXPECT_EQ(rna0.cardinality(A), rna0.cardinality(A));
+	EXPECT_EQ(rna0.cardinality(C), rna0.cardinality(C));
+	EXPECT_EQ(rna0.cardinality(G), rna0.cardinality(G));
+	EXPECT_EQ(rna0.cardinality(T), rna0.cardinality(T));
 }
 TEST(RNATest, memoryAllocation) {
 	RNA rna;
@@ -182,26 +237,16 @@ TEST(RNATest, memoryAllocation) {
 	EXPECT_EQ(rna.size(), 0);
 	EXPECT_EQ(rna.capacity(), 16);
 }
-
-/*TEST(DNATest, constructor) {
-	RNA rna0 = RNA(A, 4) + RNA(C, 3) + RNA(G, 2) + RNA(T, 1);
-	RNA rna1 = !rna0;
-	class HuckDNA: DNA {
-	public:
-		RNA& getRna0() {
-			return rna0;
-		}
-		RNA& getRna1() {
-			return rna1;
-		}
-	};
-	HuckDNA dna0(rna0, rna1);
-	HuckDNA dna1(rna0 + T, rna1);
-	HuckDNA dna2(rna0, rna1 + A);
-	HuckDNA huckDna0 =
-}*/
+TEST(RNATest, manyAddings) {
+	RNA a;
+	for (size_t i = 1; i != 1000'000'000; ++i)
+		a += A;
+}
 
 void main(int argc, wchar_t** argv) {
 	::testing::InitGoogleTest(&argc, argv);
+	_CrtMemState state1;
+	_CrtMemCheckpoint(&state1);
 	RUN_ALL_TESTS();
+	_CrtMemDumpAllObjectsSince(&state1);
 }
